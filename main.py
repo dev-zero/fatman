34dea739dd357460a14b7fcf260b761e0e27fb55
Tiziano Müller <tm@dev-zero.ch>
2016-02-12 13:08:12 +0100

    main: initialize the TaskStatus (enum) table

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from models import *
 def create_tables():
     db.create_tables([Structure, Method, Test, TestStructure, TaskStatus, Task, Results], safe=True)
 
+def initialize_tables():
+    for name in ['new', 'pending', 'running', 'done', 'error']:
+        TaskStatus.create_or_get(name=name)
+
 if __name__ == '__main__':
     create_tables()
+    initialize_tables()
     app.run()
