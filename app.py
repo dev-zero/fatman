1a75849afdfbd2f45414c0444692b01adee4d23d
Tiziano Müller <tm@dev-zero.ch>
2016-02-12 15:07:30 +0100

    app: add SECRET_KEY, required for flask-admin to edit stuff)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
 DATABASE = 'pseudotests'
+SECRET_KEY = '06ff31da-fc90-4dc6-b177-26dd0018d2d7'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
