8b19ffff957ffae6d0101f70a228e65f13dd9d86
Tiziano Müller <tm@dev-zero.ch>
2016-02-16 12:17:01 +0100

    setup: read requirements for requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='fatman',
     version='0.1.dev0',
     packages=['fatman'],
     license='GPL3',
-    install_requires=[
-        "Flask",
-        "peewee",
-        "psycopg2",
-        "Flask-RESTful",
-        "Flask-Admin",
-        "wtf-peewee",
-        "Flask-Script",
-        ]
+    install_requires=requirements,
     )
