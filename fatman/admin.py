4304e401cdd1254d13f08d1b8d4c583ebc9c1516
Tiziano Müller <tm@dev-zero.ch>
2016-02-16 11:20:02 +0100

    admin: enable export to CSV

--- fatman/admin.py
+++ fatman/admin.py
@@ -10,10 +10,12 @@ admin = Admin(app, name='FATMAN', template_mode='bootstrap3')
 class BaseView(ModelView):
     # I want the id to be displayed by default
     column_display_pk = True
+    # permit CSV export everywhere
+    can_export = True
 
 class MethodView(BaseView):
     # wtf-peewee does not know about BSJONField (yet)
-    form_excluded_columns = ('settings')
+    form_excluded_columns = ['settings']
 
 admin.add_view(BaseView(Structure))
 admin.add_view(MethodView(Method))
