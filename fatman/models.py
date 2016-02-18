3ee61dfd376a95b4a4471a863dd085a15fcb2a33
Ralph Koitz <ralph.koitz@chem.uzh.ch>
2016-02-18 14:47:57 +0000

    Modify schema  of Structure table: include field that references the structure stored by ASE.

--- fatman/models.py
+++ fatman/models.py
@@ -10,6 +10,7 @@ class BaseModel(Model):
 
 class Structure(BaseModel):
     name = CharField(unique=True, null=False)
+    ase_structure = IntegerField(unique=True, null=False)
     class Meta:
         order_by = ('name',)
 
