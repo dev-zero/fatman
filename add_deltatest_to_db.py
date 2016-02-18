d1642ba6a8ea0442e41ff7b87f32b25ea82e74a4
Ralph Koitz <ralph.koitz@chem.uzh.ch>
2016-02-18 15:34:09 +0000

    A helper script to add the required structure data and test entries for the Deltatest.

---
+++ add_deltatest_to_db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from ase.test.tasks import dcdft
+from ase.db import connect
+
+from fatman import db
+from fatman.models import Structure, Test, TestStructure
+
+def main():
+    """Add all the structures and associated tests for the DELTATEST framework to the database"""
+
+    deltastructures = dcdft.DeltaCodesDFTCollection()
+    element_list = deltastructures.keys()
+    
+    con = connect("postgresql:///fatman")
+
+    for el in element_list:
+        test, created = Test.create_or_get(name="deltatest_{}".format(el), description="Deltatest for element {}".format(el))
+
+        for strain in [0.98, 0.99, 1.00, 1.01, 1.02]:
+            data = deltastructures[el]
+
+            eq_cell = data.get_cell()
+            
+            struct = data.copy()
+            struct.set_cell(eq_cell*strain, scale_atoms=True)
+            con.write(struct, dataset="deltatest", identifier="deltatest_{}_{:4.2f}".format(el,strain))
+            row = con.get(identifier="deltatest_{}_{:4.2f}".format(el,strain))
+            ase_structure = row.id
+
+            struct, created = Structure.create_or_get(name="deltatest_{}_{:4.2f}".format(el,strain),ase_structure=ase_structure)
+            teststructure, created = TestStructure.create_or_get(structure = struct.id, test = test.id)
+
+
+if __name__=="__main__":
+    main()
