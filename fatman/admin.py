9d1c1d5302860bfba723f15b701efff8e80c0932
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-02-19 15:39:54 +0100

    admin: make JSONB field accessible via admin

--- fatman/admin.py
+++ fatman/admin.py
@@ -2,6 +2,12 @@
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.peewee import ModelView
 
+# Wire-up JSONB fields from peewee-playhouse to WTForms
+from flask_admin.contrib.peewee.form import CustomModelConverter
+from playhouse.postgres_ext import BinaryJSONField
+from wtforms import fields
+CustomModelConverter.defaults[BinaryJSONField] = fields.TextAreaField
+
 from fatman import app
 from fatman.models import *
 
@@ -16,12 +22,8 @@ class BaseView(ModelView):
     # permit CSV export everywhere
     can_export = True
 
-class MethodView(BaseView):
-    # wtf-peewee does not know about BSJONField (yet)
-    form_excluded_columns = ['settings']
-
 admin.add_view(BaseView(Structure))
-admin.add_view(MethodView(Method))
+admin.add_view(BaseView(Method))
 admin.add_view(BaseView(Test))
 admin.add_view(BaseView(TestStructure))
 admin.add_view(BaseView(Task))
