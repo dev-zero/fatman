353876f5207afd851573663ee37bf64c438d7564
Ralph Koitz <ralph.koitz@chem.uzh.ch>
2016-02-22 13:30:08 +0000

    Skeleton code for the fatman client.

---
+++ client.py
@@ -0,0 +1,30 @@
+from fatman.models import Structure, Task, TaskStructure
+from tools import Json2Atoms
+
+
+def main():
+    """The fatman client queries the DB for new tasks and runs them until none are left"""
+
+
+
+    #request a task -- and set its status to "running"
+
+    #which structure?
+
+    #which code to use?
+
+    #which settings for the code?
+
+
+
+    #run the code
+
+    #throw the energy into the DB, upload the output file
+
+
+    #update the task's status to "done"
+
+
+
+if __name__=="__main__":
+    main()
