edcbf968fe1419ae391ff3489805ea589075d6e3
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-02-22 16:24:56 +0100

    client: first shot at API usage

--- client.py
+++ client.py
@@ -1,13 +1,27 @@
-from fatman.models import Structure, Task, TaskStructure
+#!/usr/bin/env python
+
+import requests
 from tools import Json2Atoms
 
+SERVER = 'http://localhost:5000'
+TASKS_URL = SERVER + '/tasks'
 
 def main():
     """The fatman client queries the DB for new tasks and runs them until none are left"""
 
+    #request a task -- and set its status to "pending"
 
+    req = requests.get(TASKS_URL, params={'limit': 1, 'status': 'new'})
+    req.raise_for_status()
 
-    #request a task -- and set its status to "running"
+    tasks = req.json()
+    if len(tasks) == 0:
+        print('No task to run, exiting')
+        return 0
+
+    req = requests.patch(SERVER + tasks[0]['_links']['self'], data={'status': 'pending'})
+    req.raise_for_status()
+    task = req.json()
 
     #which structure?
 
@@ -19,11 +33,20 @@ def main():
 
     #run the code
 
+    #update the task's status to "running"
+    req = requests.patch(SERVER + task['_links']['self'], data={'status': 'running'})
+    req.raise_for_status()
+    task = req.json()
+
     #throw the energy into the DB, upload the output file
 
 
     #update the task's status to "done"
+    req = requests.patch(SERVER + task['_links']['self'], data={'status': 'done'})
+    req.raise_for_status()
+    task = req.json()
 
+    #upload the data
 
 
 if __name__=="__main__":
