0dab2e6a35c6579269af8b88cbbca71e8a67297a
Ralph Koitz <ralph.koitz@chem.uzh.ch>
2016-02-24 12:13:32 +0000

    add_method: changed helper script to python.

---
+++ add_method.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python
+
+from fatman.models import Pseudopotential, BasisSet, PseudopotentialFamily, BasissetFamily, Method
+
+basis  = BasissetFamily.get(BasissetFamily.name=='DZVP-MOLOPT-GTH')
+pseudo = PseudopotentialFamily.get(PseudopotentialFamily.name=='GTH-PBE')
+code   = "cp2k"
+
+Method.create_or_get(basis_set = basis, pseudopotential=pseudo, code=code, settings={"cutoff_rho":19047.976958862117, "GAPW": "false", "relativistic": "false"})
