c166a2db84dee2cdaa42d1a153b6d0b6d3df0dc8
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-02-29 14:54:16 +0100

    default_settings: add default for upload directory

--- fatman/default_settings.py
+++ fatman/default_settings.py
@@ -2,4 +2,5 @@
 DATABASE = 'fatman'
 APPLICATION_ROOT = ''
 DEBUG = False
-UPLOADED_RESULTS_ALLOW = [ 'md', 'json', 'xml', 'zip', 'tar', 'tgz', 'gz', 'tbz2', 'bz2', 'xz']
+UPLOADED_RESULTS_DEST = '/var/empty'
+UPLOADED_RESULTS_ALLOW = ['md', 'json', 'xml', 'zip', 'tar', 'tgz', 'gz', 'tbz2', 'bz2', 'xz']
