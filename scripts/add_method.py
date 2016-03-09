761924902f804180d73972976e9a598349379680
Ralph Koitz <ralph.koitz@gmail.com>
2016-03-09 10:25:54 +0100

    helper script: adding new method

--- scripts/add_method.py
+++ scripts/add_method.py
@@ -7,17 +7,22 @@ basis  = BasissetFamily.get(BasissetFamily.name=='pc-1')
 pseudo = PseudopotentialFamily.get(PseudopotentialFamily.name=='ALL')
 code   = "cp2k"
 
-Method.create_or_get(basis_set        = basis, 
+M, c = Method.create_or_get(basis_set        = basis, 
                      pseudopotential  = pseudo, 
                      code             = code, 
-                     settings         = {   "cutoff_rho"  :  1400.*Ry, 
-                                       #    "rel_settings": {"method":"ZORA",
-                                       #                     "zora_type":"MP",
-                                       #                     "transformation" : "ATOM"}  , 
-                                            "kpoint_shift": [0.5,0.5,0.5],
-                                            "qs_settings" : {"method":"GAPW",
-                                                             "extrapolation": "USE_GUESS",
-                                                             "eps_default": "1.0E-10",
-                                                             "epsiso"     : "1.0E-06"},
+                     settings         = {   "cutoff_rho"  :   1000.*Ry, 
+                                       #    "rel_settings":  {"method":"ZORA",
+                                       #                      "zora_type":"MP",
+                                       #                      "transformation" : "ATOM"}  , 
+                                            "kpoint_shift":  [0.5,0.5,0.5],
+                                            "qs_settings" :  {"method":"GAPW",
+                                                              "extrapolation": "USE_GUESS",
+                                                              "eps_default": "1.0E-10",
+                                                              "epsiso"     : "1.0E-06"},
+                                            "kind_settings": {"lebedev_grid": "80",
+                                                              "radial_grid" : "80" }
                                          }
                      )
+
+if c:
+    print "CREATED METHOD: ", M.id
