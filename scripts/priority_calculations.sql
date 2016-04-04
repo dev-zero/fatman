c49b86874d411b72f900efd1d3983c43a0be694f
Ralph Koitz <ralph.koitz@gmail.com>
2016-04-04 15:26:22 +0200

    dunno what changed here...

--- scripts/priority_calculations.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW resultwithouttestresult AS 
-SELECT result.* FROM result JOIN task a  ON a.id = result.task_id 
-    WHERE NOT EXISTS 
-    (SELECT 1 FROM testresult b 
-         WHERE a.method_id = b.method_id AND 
-               a.test_id = b.test_id
-    );
