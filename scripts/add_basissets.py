69fe2aa229bd700d89c3b7b0b0d754132173adc5
Ralph Koitz <ralph.koitz@gmail.com>
2016-05-19 14:54:05 +0200

    scripts: improve documentation and error checking in add_pseudos.py and add_basissets.py

--- scripts/add_basissets.py
+++ scripts/add_basissets.py
@@ -1,56 +1,71 @@
 #!/usr/bin/env python
+"""add_basissets.py <filename> - parse a cp2k BASIS file and add the basissets to FATMAN
+
+Use this script on the FATMAN server to populate the database with orbital basis sets
+from a file with pseudos in cp2k-compatible format.
+
+Parameters:
+    - filename: path to the basisset file to be parsed
+
+The basisset family is not automatically created in the db, thus it must exist upfront.
+The list of these 'allowed' basisset families is hardcoded ('available_bases').
+"""
+
+from sys import argv
 from fatman.models import BasissetFamily, BasisSet
+
 
 def main(args):
     fn = args[0]
 
     infile = open(fn)
-    available_bases = ['SZV-GTH', 'DZV-GTH', 'DZVP-GTH', 'TZVP-GTH', 'TZV2P-GTH', 'QZV2P-GTH', 'QZV3P-GTH', 'aug-DZVP-GTH', 'aug-TZVP-GTH', 'aug-TZV2P-GTH', 'aug-QZV2P-GTH', 'aug-QZV3P-GTH', '6-31G*', '6-311ppG3f2d', '6-31ppG3f2d', 'TZVP-pob', 'DZVP-MOLOPT-SR-GTH', 'DZVP-MOLOPT-GTH', 'DZVP-ALL', 'DZ-ANO', 'TZVP-MOLOPT-GTH', 'TZV2PX-MOLOPT-GTH', 'pc-1', 'pc-2', 'pc-3', 'pc-4']
+    available_bases = ['SZV-GTH', 'DZV-GTH', 'DZVP-GTH',
+                       'TZVP-GTH', 'TZV2P-GTH', 'QZV2P-GTH',
+                       'QZV3P-GTH', 'aug-DZVP-GTH', 'aug-TZVP-GTH',
+                       'aug-TZV2P-GTH', 'aug-QZV2P-GTH', 'aug-QZV3P-GTH',
+                       '6-31G*', '6-311ppG3f2d', '6-31ppG3f2d',
+                       'TZVP-pob', 'DZVP-MOLOPT-SR-GTH', 'DZVP-MOLOPT-GTH',
+                       'DZVP-ALL', 'DZ-ANO', 'TZVP-MOLOPT-GTH',
+                       'TZV2PX-MOLOPT-GTH', 'pc-1', 'pc-2',
+                       'pc-3', 'pc-4']
+    # only add basissets belonging to these families; must exist in
+    # the BasissetFamily table in the db.
 
     stored_basis = None
     element = None
     bases = None
 
     for line in infile:
-        if line.startswith("#") or line.startswith("!"): continue
-        
-        #find a line with an element name at the beginning
+        if line.startswith("#") or line.startswith("!"):
+            continue
+
+        # find a line with an element name at the beginning
         if line[0] not in [str(x) for x in range(10)] + [" "]:
             if element is not None and stored_basis is not None:
 
-                #here we save the previously found basis to the DB
+                # here we save the previously found basis to the DB
                 fam = [x for x in bases if x in available_bases]
-                if len(fam)==1:
+                if len(fam) == 1:
                     basis = BasissetFamily.get(name=fam[0])
 
                     b, created = BasisSet.get_or_create(family=basis, element=element, defaults=dict(basis=stored_basis))
-                    if created: print "created: ", element, fam[0]
-
-
+                    if created:
+                        print "created: ", element, fam[0]
 
             s = line.split()
             element = s[0]
             bases = s[1:]
 
-
             ok_basis = [x in available_bases for x in bases]
 
-            stored_basis=""
-                
-            continue 
+            stored_basis = ""
+
+            continue
 
         stored_basis += line
 
-if __name__=="__main__":
-    from sys import argv
-    main(argv[1:])
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    if len(argv) == 2:
+        main(argv[1:])
+    else:
+        print __doc__
--- scripts/add_pseudos.py
+++ scripts/add_pseudos.py
@@ -1,52 +1,59 @@
 #!/usr/bin/env python
-from fatman.models import PseudopotentialFamily,Pseudopotential
+"""add_pseudos.py <filename> - parse a cp2k PP file and add the pseudos to FATMAN
+
+Use this script on the FATMAN server to populate the database with pseudopotentials
+from a file with pseudos in cp2k-compatible format.
+
+Parameters:
+    - filename: path to the pseudopotential file to be parsed
+
+The PP family is not automatically created in the db, thus it must exist upfront.
+The list of these 'allowed' pseudopotential families is hardcoded ('available_pseudos').
+"""
+
+from sys import argv
+from fatman.models import PseudopotentialFamily, Pseudopotential
+
 
 def main(args):
     fn = args[0]
 
-    infile = open(fn)
-    available_pseudos= ['GTH-PBE', 'GTH-NLCC-PBE', 'GTH-NLCC2015-PBE', 'ALL']
+    available_pseudos = ['GTH-PBE', 'GTH-NLCC-PBE', 'GTH-NLCC2015-PBE', 'ALL']
+    # only add pseudos belonging to these families; must exist in
+    # the PseudopotentialFamily table in the db.
 
     stored_pseudo = None
     element = None
     pseudos = None
 
-    for line in infile:
-        if line.startswith("#") or line.startswith("!"): continue
-        
-        #find a line with an element name at the beginning
-        if line[0] not in [str(x) for x in range(10)] + [" "]:
-            if element is not None and stored_pseudo is not None:
+    with open(fn) as infile:
+        for line in infile:
+            if line.startswith("#") or line.startswith("!"):
+                continue
 
-                #here we save the previously found pseudo to the DB
-                fam = [x for x in pseudos if x in available_pseudos]
-                if len(fam)==1:
-                    pseudo = PseudopotentialFamily.get(name=fam[0])
+            # find a line with an element name at the beginning
+            if line[0] not in [str(x) for x in range(10)] + [" "]:
+                if element is not None and stored_pseudo is not None:
 
-                    Pseudopotential.create_or_get(family=pseudo, element=element, pseudo=stored_pseudo)
+                    # here we save the previously found pseudo to the DB
+                    fam = [x for x in pseudos if x in available_pseudos]
+                    if len(fam) == 1:
+                        pseudo = PseudopotentialFamily.get(name=fam[0])
 
+                        Pseudopotential.create_or_get(family=pseudo, element=element, pseudo=stored_pseudo)
 
+                s = line.split()
+                element = s[0]
+                pseudos = s[1:]
 
-            s = line.split()
-            element = s[0]
-            pseudos = s[1:]
+                stored_pseudo = ""
 
-            stored_pseudo=""
-                
-            continue 
+                continue
 
-        stored_pseudo += line
+            stored_pseudo += line
 
-if __name__=="__main__":
-    from sys import argv
-    main(argv[1:])
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    if len(argv) == 2:
+        main(argv[1:])
+    else:
+        print __doc__
