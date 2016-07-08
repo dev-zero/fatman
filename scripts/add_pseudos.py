ffc2844ca251b29ea0ba3b71887d5d40b9af8bf8
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-07-08 16:39:47 +0200

    scripts: have add_pseudos use click and add option to specify element

--- scripts/add_pseudos.py
+++ scripts/add_pseudos.py
@@ -1,61 +1,66 @@
 #!/usr/bin/env python
-"""add_pseudos.py <filename> - parse a cp2k PP file and add the pseudos to FATMAN
 
-Use this script on the FATMAN server to populate the database with pseudopotentials
-from a file with pseudos in cp2k-compatible format.
+import click
 
-Parameters:
-    <filename>  path to the pseudopotential file to be parsed.
-    -h          show this help.
-
-The PP family is not automatically created in the db, thus it must exist upfront.
-The list of these 'allowed' pseudopotential families is hardcoded ('available_pseudos').
-"""
-
-from __future__ import print_function
-from sys import argv
 from fatman.models import PseudopotentialFamily, Pseudopotential
 
+AVAILABLE_PSEUDOS = ['GTH-PBE', 'GTH-NLCC-PBE', 'GTH-NLCC2015-PBE', 'ALL']
 
-def main(args):
-    fn = args[0]
+@click.command()
+@click.argument('infile', type=click.File('r'), metavar='<potentialfile>')
+@click.argument('elements', type=str, nargs=-1, metavar='[elements..]')
+def main(infile, elements):
+    """Parse a cp2k PP file and add the pseudos to FATMAN
 
-    available_pseudos = ['GTH-PBE', 'GTH-NLCC-PBE', 'GTH-NLCC2015-PBE', 'ALL']
+    Use this script on the FATMAN server to populate the database with pseudopotentials
+    from a file with pseudos in cp2k-compatible format.
+
+    The PP family is not automatically created in the db, thus it must exist upfront.
+    The list of these 'allowed' pseudopotential families is hardcoded ('available_pseudos').
+    """
+
     # only add pseudos belonging to these families; must exist in
     # the PseudopotentialFamily table in the db.
 
-    stored_pseudo = None
+    stored_pseudo = ""
     element = None
-    pseudos = None
+    pseudos = []
 
-    with open(fn) as infile:
-        for line in infile:
-            if line.startswith("#") or line.startswith("!"):
-                continue
+    for line in infile:
+        if line.startswith("#") or line.startswith("!"):
+            continue
 
-            # find a line with an element name at the beginning
-            if line[0] not in [str(x) for x in range(10)] + [" "]:
-                if element is not None and stored_pseudo is not None:
+        # find a line with an element name at the beginning
+        if line[0] not in [str(x) for x in range(10)] + [" "]:
 
-                    # here we save the previously found pseudo to the DB
-                    fam = [x for x in pseudos if x in available_pseudos]
-                    if len(fam) == 1:
-                        pseudo = PseudopotentialFamily.get(name=fam[0])
+            # are we done parsing the entry?
+            if element is not None and len(stored_pseudo) > 0:
 
-                        Pseudopotential.create_or_get(family=pseudo, element=element, pseudo=stored_pseudo)
+                fam = [x for x in pseudos if x in AVAILABLE_PSEUDOS]
 
-                s = line.split()
-                element = s[0]
-                pseudos = s[1:]
+                # here we save the previously found pseudo to the DB
+                # if one of its aliases is in the list of pseudos
+                # and the list of wanted elements is empty (aka 'all')
+                # or the element is in the list of elements
+                if len(fam) == 1 and (not elements or element in elements):
+                    pseudofamily = PseudopotentialFamily.get(name=fam[0])
 
-                stored_pseudo = ""
+                    Pseudopotential.create_or_get(family=pseudofamily,
+                                                  element=element,
+                                                  pseudo=stored_pseudo,
+                                                  format='ABINIT')
 
-                continue
+            s = line.split()
+            element = s[0]
+            pseudos = s[1:]
 
-            stored_pseudo += line
+            stored_pseudo = ""
 
-if __name__ == "__main__":
-    if len(argv) == 2 and '-h' not in argv:
-        main(argv[1:])
-    else:
-        print (__doc__)
+            continue
+
+        stored_pseudo += line
+
+if __name__ == '__main__':
+    main()
+
+#  vim: set ts=4 sw=4 tw=0 :
