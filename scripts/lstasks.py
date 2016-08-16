beb1dc1b06bfd866966f56a98bdc217c16fffb38
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-08-16 13:02:37 +0200

    lstasks: add format parameter

--- scripts/lstasks.py
+++ scripts/lstasks.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import requests
 import click
+import collections
 from tabulate import tabulate
 
 TASKS_URL = '{}/tasks'
-TASKS_OPTS = ['status', 'limit', 'method', 'pseudofamily', 'structure']
+
+# from http://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
+def flatten(d, parent_key='', sep='/'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 @click.command()
 @click.option('--status', '-s', type=str, help='Only  list tasks with given status (default: all)')
@@ -15,16 +24,18 @@ TASKS_OPTS = ['status', 'limit', 'method', 'pseudofamily', 'structure']
 @click.option('--machine', '-m', type=str, help='Machine used for a task')
 @click.option('--pseudofamily', '-p', type=str, help='Pseudopotential family used in the method for a task')
 @click.option('--structure', '-t', type=str, help='Structure calculated')
+@click.option('--format', '-f', 'formatstring', type=str, help='Format string for each output line')
 @click.option('--url', type=str, default='https://tctdb.chem.uzh.ch/fatman',
               help='The URL where FATMAN is running (default: https://tctdb.chem.uzh.ch/fatman)')
-def lstasks(url, **kwargs):
+def lstasks(url, formatstring, **params):
     """Change state of a task"""
 
     sess = requests.Session()
     # the certificate is signed by the inofficial TC-Chem CA
     sess.verify = False
 
-    params = {o: kwargs[o] for o in kwargs.keys() if o in TASKS_OPTS and kwargs[o]}
+    # filter None values
+    params = {k: params[k] for k in params.keys() if params[k]}
     params['timeorder'] = True
 
     req = sess.get(TASKS_URL.format(url), data=params)
@@ -32,12 +43,16 @@ def lstasks(url, **kwargs):
 
     tasks = req.json()
 
-    headers = ['id', 'status', 'machine', 'mtime', 'pseudofamily', 'structure']
-    table = [[t['id'], t['status'], t['machine'], t['mtime'],
-              t['method']['pseudopotential'], t['structure']['name']]
-             for t in tasks]
+    if formatstring:
+        for task in tasks:
+            click.echo(formatstring.format(**flatten(task)))
 
-    print(tabulate(table, headers=headers))
+    else:
+        headers = ['id', 'status', 'machine', 'mtime', 'pseudofamily', 'structure']
+        table = [[t['id'], t['status'], t['machine'], t['mtime'],
+                  t['method']['pseudopotential'], t['structure']['name']]
+                 for t in tasks]
+        click.echo(tabulate(table, headers=headers))
 
 if __name__ == "__main__":
     lstasks()
