bbe57349a818ee5623044190af262045d6249108
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-10-04 11:20:47 +0200

    manage: wire-up flask-migrate into flask-script

--- manage.py
+++ manage.py
@@ -5,8 +5,9 @@ from flask_script.commands import ShowUrls, Clean
 from flask_security.script import \
         AddRoleCommand, RemoveRoleCommand, \
         CreateUserCommand, ActivateUserCommand, DeactivateUserCommand
+from flask_migrate import MigrateCommand
 
-from fatman import app, db, models, tasks
+from fatman import app, db, models, tasks, migrate
 
 manager = Manager(app)
 
@@ -29,6 +30,8 @@ manager.add_command("activate_user", ActivateUserCommand())
 # add predefined commands from flask.script
 manager.add_command("show_urls", ShowUrls())
 manager.add_command("clean", Clean())
+
+manager.add_command("db", MigrateCommand)
 
 @manager.shell
 def make_shell_context():
