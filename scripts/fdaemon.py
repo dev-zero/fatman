2ce8399e7dc9eda469644616b7dc89c055e9e6c5
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2016-11-16 18:30:02 +0100

    fdaemon: implement the direct runner

--- scripts/fdaemon.py
+++ scripts/fdaemon.py
@@ -7,6 +7,7 @@ import os
 from os import path
 from time import sleep
 from urllib.parse import urlparse
+from glob import glob
 
 import click
 import click_log
@@ -15,6 +16,65 @@ import requests
 TASKS_URL = '{}/api/v2/tasks'
 
 logger = logging.getLogger(__name__)
+
+
+def run_with_slurm(sess, server, task, task_dir):
+    subprocess.check_call(['sbatch', task['settings']['cmd']],
+                          cwd=task_dir)
+
+
+def download_only(sess, server, task, task_dir):
+    pass
+
+
+def run_direct(sess, server, task, task_dir):
+    task['data']['commands'] = {}
+    commands = task['data']['commands']
+
+    # since we block below, we set the task to running right away
+    req = sess.patch(server + task['_links']['self'],
+                     json={'status': 'running'})
+    req.raise_for_status()
+
+    for entry in task['settings']['commands']:
+        name = entry['name']
+        stdout_fn = path.join(task_dir, "{}.out".format(name))
+        stderr_fn = path.join(task_dir, "{}.err".format(name))
+
+        commands[name] = {'return': -1}
+
+        logger.info("task %s: running command %s", task['id'], name)
+
+        try:
+            with open(stdout_fn, 'w') as stdout, \
+                 open(stderr_fn, 'w') as stderr:
+                cproc = subprocess.run(
+                    [entry['cmd']] + entry['args'],
+                    stdout=stdout, stderr=stderr,
+                    cwd=task_dir)
+
+                commands[name]['return'] = cproc.returncode
+
+                if not entry.get('ignore_failure', False):
+                    cproc.check_returncode()
+
+        except EnvironmentError as error:
+            commands[name]['error'] = (
+                "error when opening stdout/stderr files: {}"
+                .format(error))
+            raise
+        except subprocess.SubprocessError as error:
+            commands[name]['error'] = (
+                "error when running: {}"
+                .format(error))
+            raise
+
+
+RUNNERS = {
+    'slurm': run_with_slurm,
+    'download-only': download_only,
+    'direct': run_direct,
+    }
 
 
 @click.command()
@@ -71,24 +131,53 @@ def main(url, hostname, nap_time, data_dir):
                 for chunk in req.iter_content(1024):
                     fhandle.write(chunk)
 
-        runner = task['settings']['machine']['runner']
-        if runner == 'slurm':
-            subprocess.check_call(['sbatch', task['settings']['cmd']],
-                                  cwd=task_dir)
-            req = sess.patch(server + task['_links']['self'],
-                             json={'status': 'running'})
-            req.raise_for_status()
-            task = req.json()
+        try:
+            runner = RUNNERS[task['settings']['machine']['runner']]
+        except KeyError:
+            raise NotImplementedError(
+                "runner {} is not (yet) implemented".format(runner))
 
-        elif runner == 'download-only':
-            req = sess.patch(server + task['_links']['self'],
-                             json={'status': 'running'})
-            req.raise_for_status()
-            task = req.json()
+        if not task['data']:
+            task['data'] = {}
 
-        else:
-            raise NotImplementedError("runner {} not yet implemented".format(
-                runner))
+        try:
+            runner(sess, server, task, task_dir)
+
+            for a_name in task['settings']['output_artifacts']:
+                full_name = path.join(task_dir, a_name)
+
+                filepaths = glob(full_name)
+
+                if not filepaths:
+                    logger.warning("task %s: glob for '%s' returned 0 files",
+                                   task['id'], a_name)
+                    # TODO: record this failure in the task
+                    continue
+
+                for filepath in filepaths:
+                    data = {
+                        'name': path.relpath(filepath, task_dir),
+                        }
+                    with open(filepath, 'rb') as data_fh:
+                        req = sess.post(server + task['_links']['uploads'],
+                                        data=data, files={'data': data_fh})
+                        req.raise_for_status()
+
+            req = sess.patch(server + task['_links']['self'],
+                             json={'status': 'done', 'data': task['data']})
+            req.raise_for_status()
+
+        except requests.exceptions.HTTPError as error:
+            logger.error("task %s: HTTP error occurred: %s\n%s",
+                         task['id'], error, error.response.text)
+
+        except Exception as error:
+            logger.error("task %s: error occurred during run: %s",
+                         task['id'], error)
+            req = sess.patch(server + task['_links']['self'],
+                             json={'status': 'error', 'data': task['data']})
+            req.raise_for_status()
+
 
 if __name__ == '__main__':
     main()
