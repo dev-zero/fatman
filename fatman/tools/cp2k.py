0908d3c5171542e252095e0737393a5cec683e27
Tiziano Müller <tm@dev-zero.ch>
2017-05-24 09:53:05 +0200

    tools/cp2k: make the CP2K output stable (sorted lexicographic, keywords before sections)

--- fatman/tools/cp2k.py
+++ fatman/tools/cp2k.py
@@ -10,12 +10,30 @@ def dict2line_iter(nested, ilevel=0):
     Iterator to convert a nested python dict to a CP2K input file.
     """
 
-    for key, val in nested.items():
+    indent = ' '*ilevel
+
+    def _keyfunc(keyval):
+        """Custom sorting function to get stable CP2K output"""
+        key, val = keyval
+
+        # ensure subsection generating entrys are sorted after keywords
+        if isinstance(val, dict) or isinstance(val, list):
+            # .. by prefixing them with the last possible character before sorting
+            return chr(0x10ffff) + key.lower()
+
+        return key.lower()
+
+    for key, val in sorted(nested.items(), key=_keyfunc):
         if isinstance(val, dict):
-            yield "{}&{} {}".format(' '*ilevel, key.upper(), val.pop('_', ''))
+            if '_' in val:
+                yield "{}&{} {}".format(indent, key.upper(), val.pop('_'))
+            else:
+                yield "{}&{}".format(indent, key.upper())
+
             for line in dict2line_iter(val, ilevel + 3):
                 yield line
-            yield "{}&END {}".format(' '*ilevel, key.upper())
+
+            yield "{}&END {}".format(indent, key.upper())
 
         elif isinstance(val, list):
             for listitem in val:
@@ -23,20 +41,21 @@ def dict2line_iter(nested, ilevel=0):
                     yield line
 
         elif isinstance(val, tuple):
-            yield "{}{} {}".format(' '*ilevel, key.upper(),
+            yield "{}{} {}".format(indent, key.upper(),
                                    ' '.join(str(v) for v in val))
 
         elif isinstance(val, bool):
             yield "{}{} {}".format(
-                ' '*ilevel, key.upper(),
+                indent, key.upper(),
                 '.TRUE.' if val else '.FALSE.')
 
         else:
-            yield "{}{} {}".format(' '*ilevel, key.upper(), val)
+            yield "{}{} {}".format(indent, key.upper(), val)
 
 
 def dict2cp2k(data, output=None, parameters={}):  # pylint: disable=locally-disabled, dangerous-default-value
-    """Convert and write a nested python dict to a CP2K input file.
+    """
+    Convert and write a nested python dict to a CP2K input file.
 
     Some of this code is either taken from AiiDA or heavily
     inspired from there.
